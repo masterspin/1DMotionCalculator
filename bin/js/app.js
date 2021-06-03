c891fd638d518762da14d7e3d410877cba4246b4
Olaf Dsouza <olafdsouza27@gmail.com>
2021-06-03 13:31:03 -0400

    fixed initial displacement

--- bin/js/app.js
+++ bin/js/app.js
@@ -4,14 +4,16 @@ document.getElementById("plot").addEventListener("click", function() {
     let accel = document.getElementById("accel").value;
 
     let time = [0,1,2,3,4,5,6,7,8,9,10];
-    
+    dist ? dist: dist= 0
 
     pos = []
     for (let i = 0; i < time.length; i++) {
-        pos[i] = (iVel * time[i]) + ((0.5 * accel) * (time[i] ** 2)) +dist;
-        console.log(0.5 * accel)
+        pos[i] = (iVel * time[i]) + ((0.5 * accel) * (time[i] ** 2)) 
         console.log("(" + time[i] + ", " + pos[i] + ")")
     };
+    console.log(dist)
+    let posi = pos.map(x => parseInt(x) + parseInt(dist))
+
 
     let position = new Chart(document.getElementById("position"), {
     type: 'line',
@@ -19,7 +21,7 @@ document.getElementById("plot").addEventListener("click", function() {
         labels: time,
         datasets: [
             { 
-            data: pos,
+            data: posi,
             label: "Displacement",
             borderColor: "#3e95cd",
             fill: false
